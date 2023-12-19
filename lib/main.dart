f5cfcfdb26ccbf07b05fc346dad4de4b67077a54
Kshitij Poudyal <kshitijpoudyal@gmail.com>
2023-12-19 14:46:02 -0500

    integrate germini api

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:flutter_gemini/flutter_gemini.dart';
+import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:lottie/lottie.dart';
 
 void main() {
+  Gemini.init(apiKey: 'AIzaSyAFXNITGCXc6C-koFwdWVZ36noAcJSc3qs');
   runApp(const MyApp());
 }
 
@@ -11,42 +17,29 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Gemini Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      title: 'Flutter Gemini',
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(
         useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ).copyWith(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          cardTheme: CardTheme(color: Colors.blue.shade900)),
+      home: const MyHomePage(title: 'Flutter Gemini'),
     );
   }
 }
 
+class SectionItem {
+  final int index;
+  final String title;
+  final Widget widget;
+
+  SectionItem(this.index, this.title, this.widget);
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +48,354 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _selectedItem = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final _sections = <SectionItem>[
+    SectionItem(0, 'text', const SectionTextInput()),
+    SectionItem(1, 'textAndImage', const SectionTextAndImageInput()),
+    SectionItem(2, 'chat', const SectionChat()),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        actions: [
+          PopupMenuButton<int>(
+            initialValue: _selectedItem,
+            onSelected: (value) => setState(() => _selectedItem = value),
+            itemBuilder: (context) => _sections.map((e) {
+              return PopupMenuItem<int>(value: e.index, child: Text(e.title));
+            }).toList(),
+            child: const Icon(Icons.more_vert_rounded),
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: IndexedStack(
+        index: _selectedItem,
+        children: _sections.map((e) => e.widget).toList(),
+      ),
+    );
+  }
+}
+
+/// -------------------------------------------------------------
+class SectionTextInput extends StatefulWidget {
+  const SectionTextInput({super.key});
+
+  @override
+  State<SectionTextInput> createState() => _SectionTextInputState();
+}
+
+class _SectionTextInputState extends State<SectionTextInput> {
+  final controller = TextEditingController();
+  final gemini = Gemini.instance;
+  String? searchedText, result;
+  bool _loading = false;
+
+  bool get loading => _loading;
+
+  set loading(bool set) => setState(() => _loading = set);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        if (searchedText != null)
+          MaterialButton(
+              color: Colors.blue.shade700,
+              onPressed: () {
+                setState(() {
+                  searchedText = null;
+                  result = null;
+                });
+              },
+              child: Text('search: $searchedText')),
+        Expanded(
+            child: loading
+                ? Lottie.asset('assets/lottie/ai.json')
+                : result != null
+                ? Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Markdown(data: result!),
+            )
+                : const Center(child: Text('Search something!'))),
+        Card(
+          margin: const EdgeInsets.all(12),
+          child: Row(
+            children: [
+              Expanded(
+                  child: TextField(
+                    controller: controller,
+                    decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 12),
+                      hintText: 'write something ...',
+                      border: InputBorder.none,
+                    ),
+                    onTapOutside: (event) =>
+                        FocusManager.instance.primaryFocus?.unfocus(),
+                  )),
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: IconButton(
+                    onPressed: () {
+                      if (controller.text.isNotEmpty) {
+                        searchedText = controller.text;
+                        controller.clear();
+                        loading = true;
+
+                        gemini.text(searchedText!).then((value) {
+                          result = value?.content?.parts?.last.text;
+                          loading = false;
+                        });
+                      }
+                    },
+                    icon: const Icon(Icons.send_rounded)),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
+/// -----------------------------------------------------
+class SectionTextAndImageInput extends StatefulWidget {
+  const SectionTextAndImageInput({super.key});
+
+  @override
+  State<SectionTextAndImageInput> createState() =>
+      _SectionTextAndImageInputState();
+}
+
+class _SectionTextAndImageInputState extends State<SectionTextAndImageInput> {
+  final ImagePicker picker = ImagePicker();
+  final controller = TextEditingController();
+  final gemini = Gemini.instance;
+  String? searchedText, result;
+  bool _loading = false;
+
+  Uint8List? selectedImage;
+
+  bool get loading => _loading;
+
+  set loading(bool set) => setState(() => _loading = set);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        if (searchedText != null)
+          MaterialButton(
+              color: Colors.blue.shade700,
+              onPressed: () {
+                setState(() {
+                  searchedText = null;
+                  result = null;
+                });
+              },
+              child: Text('search: $searchedText')),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: loading
+                      ? Lottie.asset('assets/lottie/ai.json')
+                      : result != null
+                      ? Markdown(
+                    data: result!,
+                    padding:
+                    const EdgeInsets.symmetric(horizontal: 12),
+                  )
+                      : const Center(
+                    child: Text('Search something!'),
+                  ),
+                ),
+                if (selectedImage != null)
+                  Expanded(
+                    flex: 1,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(32),
+                      child: Image.memory(
+                        selectedImage!,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  )
+              ],
+            ),
+          ),
+        ),
+        Card(
+          margin: const EdgeInsets.all(12),
+          child: Row(
+            children: [
+              Expanded(
+                  child: TextField(
+                    controller: controller,
+                    decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 12),
+                      hintText: 'write something ...',
+                      border: InputBorder.none,
+                    ),
+                    onTapOutside: (event) =>
+                        FocusManager.instance.primaryFocus?.unfocus(),
+                  )),
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: IconButton.filledTonal(
+                    color: Colors.blue,
+                    onPressed: () async {
+                      // Capture a photo.
+                      final XFile? photo =
+                      await picker.pickImage(source: ImageSource.camera);
+
+                      if (photo != null) {
+                        photo.readAsBytes().then((value) => setState(() {
+                          selectedImage = value;
+                        }));
+                      }
+                    },
+                    icon: const Icon(Icons.file_copy_outlined)),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: IconButton(
+                    onPressed: () {
+                      if (controller.text.isNotEmpty && selectedImage != null) {
+                        searchedText = controller.text;
+                        controller.clear();
+                        loading = true;
+
+                        gemini
+                            .textAndImage(
+                            text: searchedText!, image: selectedImage!)
+                            .then((value) {
+                          result = value?.content?.parts?.last.text;
+                          loading = false;
+                        });
+                      }
+                    },
+                    icon: const Icon(Icons.send_rounded)),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
+/// ---------------------------------------------------
+class SectionChat extends StatefulWidget {
+  const SectionChat({super.key});
+
+  @override
+  State<SectionChat> createState() => _SectionChatState();
+}
+
+class _SectionChatState extends State<SectionChat> {
+  final controller = TextEditingController();
+  final gemini = Gemini.instance;
+  bool _loading = false;
+
+  bool get loading => _loading;
+
+  set loading(bool set) => setState(() => _loading = set);
+  final List<Content> chats = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+            child: chats.isNotEmpty
+                ? Align(
+              alignment: Alignment.bottomCenter,
+              child: SingleChildScrollView(
+                reverse: true,
+                child: ListView.builder(
+                  itemBuilder: chatItem,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: chats.length,
+                  reverse: false,
+                ),
+              ),
+            )
+                : const Center(child: Text('Search something!'))),
+        if (loading) const CircularProgressIndicator(),
+        Card(
+          margin: const EdgeInsets.all(12),
+          child: Row(
+            children: [
+              Expanded(
+                  child: TextField(
+                    controller: controller,
+                    decoration: const InputDecoration(
+                      contentPadding: EdgeInsets.symmetric(horizontal: 12),
+                      hintText: 'write something ...',
+                      border: InputBorder.none,
+                    ),
+                    onTapOutside: (event) =>
+                        FocusManager.instance.primaryFocus?.unfocus(),
+                  )),
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: IconButton(
+                    onPressed: () {
+                      if (controller.text.isNotEmpty) {
+                        final searchedText = controller.text;
+                        chats.add(Content(
+                            role: 'user', parts: [Parts(text: searchedText)]));
+                        controller.clear();
+                        loading = true;
+
+                        gemini.chat(chats).then((value) {
+                          chats.add(Content(
+                              role: 'model',
+                              parts: [Parts(text: value?.output)]));
+                          loading = false;
+                        });
+                      }
+                    },
+                    icon: const Icon(Icons.send_rounded)),
+              )
+            ],
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget chatItem(BuildContext context, int index) {
+    final Content content = chats[index];
+
+    return Card(
+      elevation: 0,
+      color:
+      content.role == 'model' ? Colors.blue.shade800 : Colors.transparent,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(content.role ?? 'role'),
+            Markdown(
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                data:
+                content.parts?.lastOrNull?.text ?? 'cannot generate data!'),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
